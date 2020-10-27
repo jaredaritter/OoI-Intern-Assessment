5132001ffb16aaf01c4167107050fea92920db39
jared <38332939+jaredaritter@users.noreply.github.com>
2020-10-27 09:02:36 -0400

    Add formatting util to balance and cash for proper output

--- client/src/components/molecules/Card/WalletCard.jsx
+++ client/src/components/molecules/Card/WalletCard.jsx
@@ -5,6 +5,7 @@ import CardAddress from '../../atoms/Text/CardAddress';
 import CardBalance from '../../atoms/Text/CardBalance';
 import USDIcon from '../../atoms/Icons/USDIcon';
 import WalletsIcons from '../../atoms/Icons/WalletsIcon';
+import { usdFormatter, cryptoFormatter } from '../../../util/index';
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -28,6 +29,8 @@ export default function WalletCard({ children, wallet, rate }) {
   const classes = useStyles();
   console.table(rate);
   const { name, address, currency, balance } = wallet;
+  const formattedBalance = cryptoFormatter(balance);
+  const formattedCashBalance = usdFormatter.format(balance / rate);
 
   return (
     <div className={classes.card}>
@@ -36,12 +39,12 @@ export default function WalletCard({ children, wallet, rate }) {
       <CardAddress>{address}</CardAddress>
       <p>Currency: {currency}</p>
       <CardBalance>
-        {balance} {currency === 'Ethereum' ? 'ETH' : 'BTC'}
+        {formattedBalance} {currency === 'Ethereum' ? 'ETH' : 'BTC'}
       </CardBalance>
       <div>
         <CardBalance>
           <USDIcon />
-          {balance / rate}
+          {formattedCashBalance}
         </CardBalance>
       </div>
       {/* <WalletsIcons /> */}
